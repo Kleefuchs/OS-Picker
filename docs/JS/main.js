9bb906f2671adddbf8d10888595174e3b783b4a2
Kleefuchs <sam.brecht@outlook.de>
2024-06-29 11:53:40 +0200

    Added descriptions

--- docs/JS/main.js
+++ docs/JS/main.js
@@ -1,25 +1,46 @@
 
 let questionButtons = document.getElementById("question-buttons");
+let questionDescription = document.getElementById("question-description");
 let questionText = document.getElementById("question-text");
 
-let tree = {
-    "name": "undefined",
-    "text": "undefined",
-    "currentPath": "undefinded",
+//
+let questionData = {
+    "name": "",
+    "text": "",
+    "description": "",
+    "currentPath": "",
     "buttons": []
 }
 
-function setTree(name, text, buttons, path) {
-    tree.name = name;
-    tree.text = text;
-    tree.buttons = buttons;
-    tree.currentPath = path;
+function setTree(name, text, description, buttons, path) {
+    questionData.name = name;
+    questionData.text = text;
+    if(description!=undefined) {
+        questionData.description = description;
+    } else {
+        questionData.description = "";
+    }
+    if(buttons!=undefined) {
+        questionData.buttons = buttons;
+    } else {
+        questionButtons.button = [];
+    }
+    questionData.currentPath = path;
+}
+
+function setText() {
+    console.log(questionData.description);
+    questionText.innerHTML = questionData.text;
+}
+
+function setDescription() {
+    questionDescription.innerHTML = questionData.description;
 }
 
 function createButton(buttonData) {
     let button = document.createElement("button");
     button.onclick = function () {
-        init(tree.currentPath + "/" + buttonData.nextPath + "/");
+        init(questionData.currentPath + "/" + buttonData.nextPath + "/");
     };
     button.innerHTML = buttonData.text;
     return button;
@@ -27,21 +48,18 @@ function createButton(buttonData) {
 
 function addButtons() {
     questionButtons.innerHTML = "";
-    tree.buttons.forEach((buttonData) => {
+    questionData.buttons.forEach((buttonData) => {
         questionButtons.appendChild(createButton(buttonData));
     });
-}
-
-function setText() {
-    questionText.innerHTML = tree.text;
 }
 
 async function init(path) {
     console.log(path)
     await $.getJSON(path + "/index.json", function (data) {
-        setTree(data.name, data.text, data.buttons, path);
+        setTree(data.name, data.text, data.description, data.buttons, path);
     });
     await setText();
+    await setDescription();
     await addButtons();
 }
 
